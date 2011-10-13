cbdc62ba070ab71143dfc3b06bc4f3037e5e12bd
Sean Fisk <sean@seanfisk.com>
2011-10-13 06:17:39 -0400

    Added a lot of code, not sure if it works.

--- client/src/main/java/edu/gvsu/cis/cis656/lab2/ChatClient.java
+++ client/src/main/java/edu/gvsu/cis/cis656/lab2/ChatClient.java
@@ -3,22 +3,39 @@
  */
 package edu.gvsu.cis.cis656.lab2;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.rmi.AccessException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.LinkedHashMap;
+import java.util.Scanner;
 
 /**
  * @author Sean Fisk <fiskse@mail.gvsu.edu>
  */
 public class ChatClient
 {
+	private PresenceService presenceService = null;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args)
 	{
+		new ChatClient().go(args);
+	}
+	
+	void go(String[] args)
+	{
+		// check number of args
 		if(args.length < 1 || args.length > 2 || args[0].equalsIgnoreCase("--help") || args[0].equalsIgnoreCase("-h"))
 			usage();
 		
+		// parse args
 		String username = args[0];
 		String host = null;
 		int port = 0;
@@ -47,11 +64,14 @@ public class ChatClient
 				}
 			}
 		}
-		
+
+		// set up security manager if it doesn't exist
 		if(System.getSecurityManager() == null)
 			System.setSecurityManager(new SecurityManager());
+		
 		try
 		{
+			// get registry
 			// make absolutely sure we always get the JVM defaults (and don't just supply them)
 			Registry registry;
 			if(host != null && port != 0)
@@ -61,16 +81,81 @@ public class ChatClient
 			else
 				registry = LocateRegistry.getRegistry();
 			
-			String name = "PresenceService";
-			PresenceService presenceService = (PresenceService)registry.lookup(name);
+			// get the handle to the presence service
+			presenceService = (PresenceService)registry.lookup("PresenceService");
 			
-			RegistrationInfo reg = new RegistrationInfo(username, "localhost", 0, true);
-			System.out.println("Success? " + presenceService.register(reg));
+			// bind the server socket behind the message listener
+			MessageListener messageListener = new MessageListener();
+			
+			// set up registration info
+			RegistrationInfo reg = new RegistrationInfo(
+					username,
+					messageListener.getInetAddress().getHostAddress(),
+					messageListener.getLocalPort(),
+					true);
+			
+			// register with the presence service
+			if(!presenceService.register(reg))
+			{
+				System.err.println("Sorry, the name `" + username + "' is taken.");
+				System.exit(1);
+			}
+			
+			// start the message listener
+			new Thread(messageListener).start();
+			
+			// add available commands
+			LinkedHashMap<String, Command> commands = new LinkedHashMap<String, Command>();
+			commands.put("friends", new FriendsCommand());
+			commands.put("talk", new TalkCommand());
+			commands.put("quit", new QuitCommand());
+			
+			// print out command list
+			for(Command value : commands.values())
+				System.out.println(value);
+						
+			// enter command loop
+			Scanner scanner = new Scanner(System.in);
+			
+			while(true)
+			{
+				// grab command
+				Command command = commands.get(scanner.next());
+				
+				// check invalid
+				if(command == null)
+				{
+					System.err.println("Invalid command.");
+					continue;
+				}
+				
+				// quit on quit command (cannot `break' in a class)
+				if(command instanceof QuitCommand)
+					break;
+				
+				// execute command
+				command.execute(scanner.nextLine());
+			}
+			
+			// close the listening thread
+			messageListener.close();
 		}
-		catch(Exception e)
+		catch(AccessException e)
 		{
-			System.err.println("PresenceService exception:");
+			System.err.println("Cannot access RMI resource.");
 			e.printStackTrace();
+			System.exit(1);
+		}
+		catch(RemoteException e)
+		{
+			System.err.println("RMI error.");
+			e.printStackTrace();
+			System.exit(1);
+		}
+		catch(NotBoundException e)
+		{
+			e.printStackTrace();
+			System.exit(1);
 		}
 	}
 
@@ -78,5 +163,88 @@ public class ChatClient
 	{
 		System.err.println("Usage: java edu.gvsu.cis.cis656.lab2.ChatClient user [host[:port]]");
 		System.exit(1);
+	}
+	
+	// commands
+	private interface Command
+	{
+		public void execute(String args) throws RemoteException;
+		public String toString();
+	}
+		
+	// friends
+	private class FriendsCommand implements Command
+	{
+		public void execute(String args) throws RemoteException
+		{
+			System.out.println(presenceService.listRegisteredUsers());
+		}
+		
+		public String toString()
+		{
+			return "friends						list registered users and their availability";
+		}
+	}
+	
+	// quit
+	private class QuitCommand implements Command
+	{
+		public void execute(String args) throws RemoteException
+		{
+			
+		}
+		
+		public String toString()
+		{
+			return "quit						Exit the chat client";
+		}
+	}
+	
+	// talk
+	private class TalkCommand implements Command
+	{
+		public void execute(String args) throws RemoteException
+		{
+			Scanner scanner = new Scanner(args);
+			String username = scanner.next();
+			String message = scanner.nextLine();
+			RegistrationInfo reg = presenceService.lookup(username);
+			
+			if(reg == null)
+			{
+				System.out.println("User `" + username + "' isn't registered on this server.");
+				return;
+			}
+			
+			if(!reg.getStatus())
+			{
+				System.out.println("User `" + username + "' isn't available right now.");
+				return;
+			}
+			
+			try
+			{
+				Socket socket = new Socket(reg.getHost(), reg.getPort());
+				PrintWriter out = new PrintWriter(socket.getOutputStream());
+				out.println(message);
+				out.close();
+				socket.close();
+			}
+			catch(UnknownHostException e)
+			{
+				System.err.println("Unknown host.");
+				e.printStackTrace();
+			}
+			catch(IOException e)
+			{
+				e.printStackTrace();
+			}
+			
+		}
+		
+		public String toString()
+		{
+			return "talk {username} {message}	send a message to another user";
+		}
 	}
 }
---
+++ client/src/main/java/edu/gvsu/cis/cis656/lab2/MessageListener.java
@@ -0,0 +1,91 @@
+/**
+ * 
+ */
+package edu.gvsu.cis.cis656.lab2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
+/**
+ * @author Sean Fisk <fiskse@mail.gvsu.edu>
+ *
+ */
+public class MessageListener implements Runnable
+{
+	private ServerSocket serverSocket;
+	
+	public MessageListener()
+	{
+		try
+		{
+			serverSocket = new ServerSocket(0);
+		}
+		catch(IOException e)
+		{
+			System.err.println("Error binding the listening socket.");
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+	
+	public InetAddress getInetAddress()
+	{
+		return serverSocket.getInetAddress();
+	}
+	
+	public int getLocalPort()
+	{
+		return serverSocket.getLocalPort();
+	}
+	
+	public void close()
+	{
+		try
+		{
+			serverSocket.close();
+		}
+		catch(IOException e)
+		{
+			System.err.println("Error closing the server socket.");
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override public void run()
+	{
+		try
+ 		{
+ 			while(true)
+ 			{
+ 				Socket sock = serverSocket.accept();
+ 				BufferedReader reader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+ 				String line;
+ 				while((line = reader.readLine()) != null)
+ 					System.out.print(line + "\n> ");
+ 				
+ 				reader.close();
+ 				sock.close();
+ 			}
+ 		}
+ 		catch (SocketException e)
+ 		{
+ 			// socket was closed
+ 			return;
+ 		}
+		catch(IOException e)
+		{
+			System.err.println("Error in receiving messages.");
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+}
