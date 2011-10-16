8011761dca37cc958a7333b932757c27e0165f1a
Sean Fisk <sean@seanfisk.com>
2011-10-16 02:51:55 -0400

    Don't print out the table of friends if noone else is registered.

--- client/src/main/java/edu/gvsu/cis/cis656/lab2/command/FriendsCommand.java
+++ client/src/main/java/edu/gvsu/cis/cis656/lab2/command/FriendsCommand.java
@@ -1,6 +1,7 @@
 package edu.gvsu.cis.cis656.lab2.command;
 
 import java.rmi.RemoteException;
+import java.util.Vector;
 
 import edu.gvsu.cis.cis656.lab2.PresenceService;
 import edu.gvsu.cis.cis656.lab2.RegistrationInfo;
@@ -14,6 +15,12 @@ public class FriendsCommand extends Command
 
 	public void execute(String args) throws RemoteException
 	{
+		Vector<RegistrationInfo> registeredUsers = presenceService.listRegisteredUsers();
+		if(registeredUsers.size() == 1)
+		{
+			System.out.println("Noone else is currently registered...");
+			return;
+		}
 		final int COLUMN_WIDTH = 20;
 		final String TWO_COLUMN_FORMAT = "%" + -COLUMN_WIDTH + "s%" + -COLUMN_WIDTH + "s\n";
 		System.out.println();
@@ -21,7 +28,7 @@ public class FriendsCommand extends Command
 		for(int i = 0; i < 2 * COLUMN_WIDTH; ++i)
 			System.out.print('-');
 		System.out.println('\n');
-		for(RegistrationInfo reg : presenceService.listRegisteredUsers())
+		for(RegistrationInfo reg : registeredUsers)
 			if(!regInfo.getUserName().equals(reg.getUserName()))
 				System.console().printf(TWO_COLUMN_FORMAT, reg.getStatus() ? "available" : "busy", reg.getUserName());
 		System.out.println();
